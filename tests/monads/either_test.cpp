eeac1bc90aaece0d66198a193f42a72b0c9eb6ad
wmbat <wmbat@protonmail.com>
2020-07-28 16:33:00 -0400

    Fixing more errors in unit test

--- tests/monads/either_test.cpp
+++ tests/monads/either_test.cpp
@@ -16,7 +16,7 @@ TEST_F(either_suite, left_copy_ctor)
 
       either<int, std::string> left_either{temp};
 
-      EXPECT_EQ(left_either.is_right(), true);
+      EXPECT_EQ(left_either.is_right(), false);
       EXPECT_EQ(left_either.left().has_value(), true);
       EXPECT_EQ(left_either.left().value(), 0);
    }
@@ -29,14 +29,14 @@ TEST_F(either_suite, left_move_ctor)
 
       either<int, std::string> left_either{left_t<int>{9}};
 
-      EXPECT_EQ(left_either.is_right(), true);
+      EXPECT_EQ(left_either.is_right(), false);
       EXPECT_EQ(left_either.left().has_value(), true);
       EXPECT_EQ(left_either.left().value(), 9);
    }
    {
       either<std::unique_ptr<int>, std::string> e{make_left(std::make_unique<int>(10))};
 
-      EXPECT_EQ(e.is_right(), true);
+      EXPECT_EQ(e.is_right(), false);
       EXPECT_EQ(std::move(e).left().has_value(), true);
    }
 }
