0459a8e3f2653dcda270c52435f6812826365ee3
wmbat <wmbat@protonmail.com>
2021-06-24 21:10:11 -0400

    Rebasing on master

--- tests/basic/main.cpp
+++ tests/basic/main.cpp
@@ -1,5 +1,9 @@
 #define LIBREGLISSE_USE_EXCEPTIONS
+#include <libreglisse/either.hpp>
 #include <libreglisse/maybe.hpp>
+#include <libreglisse/ref.hpp>
+#include <libreglisse/result.hpp>
+#include <libreglisse/try.hpp>
 
 #include <exception>
 #include <string>
